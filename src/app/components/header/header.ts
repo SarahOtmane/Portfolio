7717dc2e61634ab59284178b483344402ac1913f
SarahOtmane <sarahotmane02@gmail.com>
2025-12-01 20:57:20 +0100

    Ajout de la fonctionnalité de menu dans le composant Header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  isMenuOpen = false;
+
+  menuItems = [
+    { label: 'About', route: '/' },
+    { label: 'Experiences', route: '/experiences' },
+    { label: 'Projects', route: '/projects' },
+    { label: 'Skills', route: '/skills' },
+    { label: 'Contact', route: '/contact' }
+  ];
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
