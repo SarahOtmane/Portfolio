1361aa7da0cf3836779ed7b9d9cb5c31bc6a6c7c
SarahOtmane <sarahotmane02@gmail.com>
2025-12-03 00:13:00 +0100

    feat: ajouter le composant SectionContact dans la page d'accueil

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SectionProjet } from '../../components/section-projet/section-projet';
 import { SectionAlternance } from '../../components/section-alternance/section-alternance';
+import { SectionContact } from '../../components/section-contact/section-contact';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SectionProjet, SectionAlternance],
+  imports: [SectionProjet, SectionAlternance, SectionContact],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
