1cac0fddf8e2d4cedd034c2d6118d5b2b288209a
SarahOtmane <sarahotmane02@gmail.com>
2025-12-03 00:16:58 +0100

    feat: ajouter le composant SectionSkills avec ses fichiers associés

---
+++ src/app/components/section-skills/section-skills.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-skills',
+  imports: [],
+  templateUrl: './section-skills.html',
+  styleUrl: './section-skills.css',
+})
+export class SectionSkills {
+
+}
