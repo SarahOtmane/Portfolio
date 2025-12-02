fa078c2647e2cedee84597d7a5d3f0f2c47dfdc8
SarahOtmane <sarahotmane02@gmail.com>
2025-12-03 00:18:24 +0100

    feat: ajouter SectionSkills aux imports du composant Home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { SectionProjet } from '../../components/section-projet/section-projet';
 import { SectionAlternance } from '../../components/section-alternance/section-alternance';
 import { SectionContact } from '../../components/section-contact/section-contact';
+import { SectionSkills } from '../../components/section-skills/section-skills';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SectionProjet, SectionAlternance, SectionContact],
+  imports: [SectionProjet, SectionAlternance, SectionContact, SectionSkills],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
