8d5d6715aec6274fec2d6729b6f60db33d244d12
SarahOtmane <sarahotmane02@gmail.com>
2025-12-03 00:12:55 +0100

    feat: ajouter le composant SectionContact avec HTML, CSS et tests

---
+++ src/app/components/section-contact/section-contact.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-contact',
+  imports: [],
+  templateUrl: './section-contact.html',
+  styleUrl: './section-contact.css',
+})
+export class SectionContact {
+
+}
