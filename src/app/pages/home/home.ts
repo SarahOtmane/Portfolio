a9776fdcbf7c7d850ab819333b46099836a25e74
SarahOtmane <sarahotmane02@gmail.com>
2025-12-02 23:11:56 +0100

    feat: ajuster le style des sections et intégrer le composant SectionAlternance

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SectionProjet } from '../../components/section-projet/section-projet';
+import { SectionAlternance } from '../../components/section-alternance/section-alternance';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SectionProjet],
+  imports: [SectionProjet, SectionAlternance],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
