b1a533218b983320ece56f6af41c77a63aa83e6e
SarahOtmane <sarahotmane02@gmail.com>
2025-12-10 13:50:07 +0100

    feat: ajouter la liste des catégories de compétences à la section skills

--- src/app/components/section-skills/section-skills.ts
+++ src/app/components/section-skills/section-skills.ts
@@ -2,10 +2,36 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-section-skills',
+  standalone: true,
   imports: [],
   templateUrl: './section-skills.html',
   styleUrl: './section-skills.css',
 })
 export class SectionSkills {
-
+  skillsCategories = [
+    {
+      title: 'Frontend',
+      skills: ['ReactJs', 'TypeScript', 'Angular', 'Tailwind']
+    },
+    {
+      title: 'Backend',
+      skills: ['NodeJs', 'NestJs', 'Express', 'Python', 'Django', 'Flask', 'Java', 'Spring Boot']
+    },
+    {
+      title: 'DevOps',
+      skills: ['Docker', 'Kubernetes', 'AWS', 'Azure', 'Git', 'CI/CD']
+    },
+    {
+      title: 'Databases',
+      skills: ['MySQL', 'PostgreSQL', 'MongoDB', 'Redis', 'Firebase']
+    },
+    {
+      title: 'Supervision',
+      skills: ['Prometheus', 'Grafana', 'ELK Stack']
+    },
+    {
+      title: 'Gestion projet',
+      skills: ['Jira', 'Confluence', 'Asana']
+    }
+  ];
 }
